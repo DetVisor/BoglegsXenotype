b0874716d26c9fbc26752609449465c12df44bd3
Dexter Lowe <feldoh10@gmail.com>
2025-08-03 02:37:49 +0100

    1.6 First pass
    Migration to new project type, removal of publiciser, refactor and 1.6 recompile

---
+++ Source/Boglegs/HediffCompProperties_Fat.cs
@@ -0,0 +1,33 @@
+﻿using Verse;
+
+namespace Boglegs
+{
+    public class HediffCompProperties_Fat : HediffCompProperties
+    {
+        // Amount of nutrition past which fat is accumulated
+        public float startThreshold;
+        // Conversion rate between fat and nutrition when accumulating (when using fat, conversion rate is always 1)
+        // Higher number will mean that more nutrition is converted into less fat
+        public float conversionRate;
+        // Amount of nutrition below which fat will be spent
+        public float starvationThreshold;
+        // Rate at which fat is accumulated, fat units per tick
+        public float conversionSpeed;
+        // Rate at which fat is spent, fat units per tick
+        public float starvationSpeed;
+        // Maximum amount of nutrition that can be stored
+        public float maxFat;
+
+        // Curve for slowdown, 1 = 100% movespeed
+        public SimpleCurve slowdownCurve;
+        // Curve for blunt protection, 1 = 100% blunt protection
+        public SimpleCurve bluntProtectionCurve;
+        // Curve for additional meat amount
+        public SimpleCurve meatCurve;
+
+        public HediffCompProperties_Fat()
+        {
+            compClass = typeof(HediffComp_Fat);
+        }
+    }
+}
---
+++ Source/Boglegs/HediffFat.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace Boglegs
+{
+    public class HediffFat : HediffWithComps
+    {
+        private HediffComp_Fat cachedComp;
+        private HediffComp_Fat Comp => cachedComp ?? (cachedComp = this.TryGetComp<HediffComp_Fat>());
+
+        private float cachedFat = -1f;
+        private HediffStage cachedStage;
+
+        public override HediffStage CurStage
+        {
+            get
+            {
+                if (Mathf.Approximately(cachedFat, Comp.storedFat))
+                {
+                    return cachedStage;
+                }
+
+                cachedFat = Comp.storedFat;
+                cachedStage = new HediffStage
+                {
+                    capMods = new List<PawnCapacityModifier>
+                    {
+                        new PawnCapacityModifier { capacity = PawnCapacityDefOf.Moving, offset = Comp.Props.slowdownCurve.Evaluate(cachedFat) }
+                    },
+                    statOffsets = new List<StatModifier>
+                    {
+                        new StatModifier { stat = StatDefOf.ArmorRating_Blunt, value = Comp.Props.bluntProtectionCurve.Evaluate(cachedFat) },
+                        new StatModifier { stat = StatDefOf.MeatAmount, value = Comp.Props.meatCurve.Evaluate(cachedFat) }
+                    }
+                };
+
+                return cachedStage;
+            }
+        }
+
+        public override void Notify_PawnDied(DamageInfo? dinfo, Hediff culprit = null)
+        {
+            base.Notify_PawnDied(dinfo, culprit);
+            Comp.cachedNeed = null;
+        }
+    }
+}
